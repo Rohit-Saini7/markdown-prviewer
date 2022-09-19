10b72e73be0b0af54b6d6d2849c0bfb883483c0b
rohit.saini1 <rohit.saini@valyu.ai>
2022-09-19 23:30:44 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,93 @@
+import { marked } from 'marked';
+import Prism from 'prismjs';
+import { useState } from 'react';
+import { placeholder } from './placeholder';
+
+marked.setOptions({
+  breaks: true,
+  highlight: function (code) {
+    return Prism.highlight(code, Prism.languages.javascript, 'javascript');
+  },
+});
+
+const renderer = new marked.Renderer();
+renderer.link = function (href, title, text) {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
 function App() {
-  return <div className='App'>app</div>;
+  const [markdown, setMarkdown] = useState(placeholder);
+  const [editorMaximized, setEditorMaximized] = useState(false);
+  const [previewMaximized, setPreviewMaximized] = useState(false);
+
+  function handleChange(e) {
+    setMarkdown(e.target.value);
+  }
+  function handleEditorMaximize() {
+    setEditorMaximized((prev) => !prev);
+  }
+  function handlePreviewMaximize() {
+    setPreviewMaximized((prev) => !prev);
+  }
+  const classes = editorMaximized
+    ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress']
+    : previewMaximized
+    ? ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress']
+    : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
+  return (
+    <div>
+      <div className={classes[0]}>
+        <Toolbar
+          icon={classes[2]}
+          onClick={handleEditorMaximize}
+          text='Editor'
+        />
+        <Editor markdown={markdown} onChange={handleChange} />
+      </div>
+      <div className={classes[1]}>
+        <Toolbar
+          icon={classes[2]}
+          onClick={handlePreviewMaximize}
+          text='Previewer'
+        />
+        <Preview markdown={markdown} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
+
+const Toolbar = (props) => {
+  return (
+    <div className='toolbar'>
+      <i className='fa fa-free-code-camp' title='no-stack-dub-sack' />
+      {props.text}
+      <i className={props.icon} onClick={props.onClick} />
+    </div>
+  );
+};
+
+const Editor = (props) => {
+  return (
+    <textarea
+      id='editor'
+      onChange={props.onChange}
+      type='text'
+      value={props.markdown}
+    />
+  );
+};
+
+// /*
+const Preview = (props) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: marked(props.markdown, { renderer: renderer }),
+      }}
+      id='preview'
+    />
+  );
+};
+//  */
---
+++ src/placeholder.js
@@ -0,0 +1,46 @@
+export const placeholder = `# Welcome to Markdown Previewer!
+
+# Headings
+
+# Heading level 1
+## Heading level 2
+### Heading level 3
+#### Heading level 4
+##### Heading level 5
+###### Heading level 6
+
+
+# Paragraphs 
+
+I really like using Markdown.
+
+I think I'll use it to format all of my documents from now on.
+
+
+# Line breaks
+
+This is the first line.  
+And this is the second line. 
+
+
+# Emphasis
+
+I just love **bold text**.
+
+Italicized text is the *cat's meow*.
+
+This text is ***really important & bold and italic at the same time***.
+
+
+# Blockquotes
+
+> To create a blockquote, add a > in front of a paragraph.
+
+## Nested Blockquotes
+
+> Dorothy followed her through many of the beautiful rooms in her castle.
+>
+>> The Witch bade her clean the pots and kettles and sweep the floor and keep the fire fed with wood.
+
+
+`;
